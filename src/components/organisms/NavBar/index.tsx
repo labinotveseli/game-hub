2fef9c2688940822be7d9e151762d8b91f5e7d5e
Labinot <labinotveseli1@gmail.com>
2023-08-26 18:44:56 +0200

    colormode switch

--- src/components/organisms/NavBar/index.tsx
+++ src/components/organisms/NavBar/index.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import Logo from "../../atoms/images/logo.webp";
+import ColorModeSwitch from "../ColorMode";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={Logo} boxSize="60px" />
-      <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
